d0a8fdb5a1957389e57aa5655dc20663eba4019e
Ehya-Ag <ehyaagmohamed23@gmail.com>
2026-06-02 10:25:24 +0000

    Add menu options for managing expenses

--- depenses.py
+++ depenses.py
@@ -4,5 +4,10 @@
 # ============================================
 
 print("Bienvenue dans mon gestionnaire de dépenses !")
-nom = input("Quel est votre nom ? ")
-print("Bonjour, " + nom + " !")
+
+print("1. Ajouter une dépense")
+print("2. Voir les dépenses")
+print("3. Quitter")
+
+choix = input("Veuillez choisir une option (1, 2 ou 3) : ")
+print("Vous avez choisi l'option :" + choix)
