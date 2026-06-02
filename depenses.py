6f759f3986ae262fd9076c0a4187c7e5025eccbf
Ehya-Ag <ehyaagmohamed23@gmail.com>
2026-06-02 10:20:46 +0000

    First commit

---
+++ depenses.py
@@ -0,0 +1,8 @@
+# ============================================
+#   GESTIONNAIRE DE DÉPENSES
+#   Mon projet pour apprendre Python
+# ============================================
+
+print("Bienvenue dans mon gestionnaire de dépenses !")
+nom = input("Quel est votre nom ? ")
+print("Bonjour, " + nom + " !")
