aa2b309ec5f51221077b50321264d14b7367e9d3
Ehya-Ag <ehyaagmohamed23@gmail.com>
2026-06-03 13:14:24 +0000

    Refactor expense management options to use a loop for user interaction

--- depenses.py
+++ depenses.py
@@ -5,18 +5,34 @@
 
 print("Bienvenue dans mon gestionnaire de dépenses !")
 
-print("1. Ajouter une dépense")
-print("2. Voir les dépenses")
-print("3. Quitter")
+# print("1. Ajouter une dépense")
+# print("2. Voir les dépenses")
+# print("3. Quitter")
 
-choix = input("Veuillez choisir une option (1, 2 ou 3) : ")
-print("Vous avez choisi l'option :" + choix)
+# choix = input("Veuillez choisir une option (1, 2 ou 3) : ")
+# print("Vous avez choisi l'option :" + choix)
 
-if choix == "1":
-    print("Ajouter une dépense")
-elif choix == "2":
-    print("Voir les dépenses")
-elif choix == "3":
-    print("Au revoir !")
-else:
-    print("Choix invalide")
+# if choix == "1":
+#     print("Ajouter une dépense")
+# elif choix == "2":
+#     print("Voir les dépenses")
+# elif choix == "3":
+#     print("Au revoir !")
+# else:
+#     print("Choix invalide")
+
+while True:
+    print("1. Ajouter une dépense")
+    print("2. Voir les dépenses")
+    print("3. Quitter")
+    choix = input("Veuillez choisir une option (1, 2 ou 3) : ")
+
+    if choix == "1":
+        print("Ajouter une depense")
+    elif choix == "2":
+        print("Voir les depenses")
+    elif choix == "3":
+        print("Au revoir !")
+        break
+    else:
+        print("Choix invalide")
